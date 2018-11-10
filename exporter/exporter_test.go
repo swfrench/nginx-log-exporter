cf85c233cf06f7c59f6b19366d8122e121f0ea5d
Scott W French <scott.w.french@gmail.com>
2018-11-10 11:11:44 -0800

    Fix test failure in exporter
    It is illegal re-register a metric descriptor with the same fully qualified name but different labels [0]. Also ensures that unregistration succeeds at the end of each test case.
    
    [0] https://github.com/prometheus/client_golang/blame/master/prometheus/registry.go#L382

--- exporter/exporter_test.go
+++ exporter/exporter_test.go
@@ -20,13 +20,14 @@ func TestStatusCounterCreationTime(t *testing.T) {
 	if creationTime := e.StatusCounterCreationTime(); creationTime.Before(tMin) || creationTime.After(tMax) {
 		t.Fatalf("Reported counter creation time of %v is not in [%v, %v]", creationTime, tMin, tMax)
 	}
-	prometheus.Unregister(e.StatusCounterMetric())
+	if !prometheus.Unregister(e.StatusCounterMetric()) {
+		t.Fatalf("Failed to unregister the exported metric.")
+	}
 }
 
 func TestBasicStatusCounterUpdates(t *testing.T) {
 	e := exporter.NewExporter(map[string]string{
 		"foo": "bar",
-		"bin": "baz",
 	})
 
 	e.IncrementStatusCounter(map[string]float64{
@@ -42,13 +43,15 @@ func TestBasicStatusCounterUpdates(t *testing.T) {
 	const expected = `
 		# HELP http_response_count Counts of responses by status code
 		# TYPE http_response_count counter
-		http_response_count{foo="bar", bin="baz", status_code="200"} 2
-		http_response_count{foo="bar", bin="baz", status_code="403"} 1
-		http_response_count{foo="bar", bin="baz", status_code="500"} 3
+		http_response_count{foo="bar", status_code="200"} 2
+		http_response_count{foo="bar", status_code="403"} 1
+		http_response_count{foo="bar", status_code="500"} 3
 	`
 
 	if err := testutil.CollectAndCompare(e.StatusCounterMetric(), strings.NewReader(expected)); err != nil {
 		t.Errorf("Collected metrics and / or metadata do not match expectation:\n%s", err)
 	}
-	prometheus.Unregister(e.StatusCounterMetric())
+	if !prometheus.Unregister(e.StatusCounterMetric()) {
+		t.Fatalf("Failed to unregister the exported metric.")
+	}
 }
